2ebf218da7eb522bf187ec87762efa0caacbb3ec
Mateusz Budnik <mat.budnik@outlook.com>
2020-03-17 13:43:20 +0100

    possibility to send files with socket

--- network.py
+++ network.py
@@ -1,28 +1,36 @@
 import socket
 
-def send(host: str, data, port: int = 65432):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            conn.send(data)
+def send(host: str, data, port: int = 8080):
+    """
+    Implementation of client
+    It send data to the other computer
+    :param host: ip address of the receiver
+    :param port: port
+    """
+    with socket.socket() as s:
+        s.connect((host, port))
+        s.send(data)
 
-def receive(host: str, port: int = 65432):
+def receive(host: str = '0.0.0.0', port: int = 8080):
     """
-    Call that function in thread
+    Implementation of server
+    It in infinite loop waits for connection and when the connection will
+    be established receive the data and write it to the file
+    :param host: ip address of the sender (default 0.0.0.0 what's mean all 
+        IPv4 addresses on the local machine)
+    :param port: port
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket() as s:
+        s.bind((host, port))
+        s.listen(5)
         while True:
-            print('ok')
-            s.bind((host, port))
-            s.listen()
             conn, addr = s.accept()
+            received = ''
+            print('Got connection from ', addr)
             with conn:
-                print('Connected by', addr)
                 while True:
                     data = conn.recv(1024)
-                    if not data:
-                        break
+                    if not data: break
+                    received += data.decode('utf-8')
             with open('received_file.txt', 'w') as file:
-                file.write(data)
+                file.write(received)
