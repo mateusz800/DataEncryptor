548959d44fff694604f16ba6b191910b4fc4c1a6
Mateusz Budnik <mat.budnik@outlook.com>
2020-03-18 13:33:56 +0100

    Stop receive thread when exit

--- application.py
+++ application.py
@@ -1,8 +1,10 @@
 import tkinter as tk
+from tkinter.ttk import Progressbar
 from file_chooser import FileChooser
 from key import Key
 from file import File
-from network import send, receive
+from network import send, ReceiveThread
+from progress import Progress
 import sys
 import threading
 
@@ -20,13 +22,13 @@ class Application:
         self._window = tk.Tk()
         self._key = Key(length=16)
         self._init_vector = Key(length=16)
-        self._file_chooser = FileChooser()
         self._window.geometry('{}x{}'.format(width, height))
         self._create_widgets()
 
+        self._window.protocol("WM_DELETE_WINDOW", self._exit)
         self._window.grid_columnconfigure(1, weight=1)
-        receiver = threading.Thread(target=receive, args=('0.0.0.0',))
-        receiver.start()
+        self._receive_thread = ReceiveThread(host='0.0.0.0')
+        self._receive_thread.start()
 
     def run(self):
         self._window.mainloop()
@@ -35,12 +37,16 @@ class Application:
         """
         Creating widgets and putting them into screen
         """
+        self._file_chooser = FileChooser()
+
         self._generate_key_btn = tk.Button(
             text='generate key', command=self._generate_key)
         self._generate_key_btn.grid()
         self._send_btn = tk.Button(
             text='send encrypted file', command=self._encrypt)
         self._send_btn.grid()
+
+        self._progress = Progress()
 
     def _generate_key(self):
         """
@@ -56,3 +62,11 @@ class Application:
         file.encrypt(key=self._key.key, iv=self._init_vector.key)
         file.decrypt(key=self._key.key, iv=self._init_vector.key)
         send('0.0.0.0', file.get_data.encode('utf-8'))
+
+    def _exit(self):
+        """
+        Close the window and stop all threads running in background
+        """
+        self._window.destroy()
+        self._receive_thread.stop()
+        self._receive_thread.join()
--- network.py
+++ network.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 
 def send(host: str, data, port: int = 8080):
     """
@@ -9,29 +11,49 @@ def send(host: str, data, port: int = 8080):
     :param int port: port, defaults to 8080
     """
     with socket.socket() as s:
-        s.connect((host, port))
-        s.send(data)
+        self._socket.connect((host, port))
+        self._socket.send(data)
 
-def receive(host: str = '0.0.0.0', port: int = 8080):
-    """
-    Implementation of server.
-    It in infinite loop waits for connection and when the connection will
-    be established receive the data and write it to the file.
 
-    :param str host: ip address of the sender ,default to 0.0.0.0 what's mean all IPv4 addresses on the local machine)
-    :param int port: port, defaults to 8080
-    """
-    with socket.socket() as s:
-        s.bind((host, port))
-        s.listen(5)
-        while True:
-            conn, addr = s.accept()
-            received = ''
+class ReceiveThread(threading.Thread):
+    def __init__(self, host: str = '0.0.0.0', port: int = 8080):
+        """
+        :param str host: ip address of the sender ,default to 0.0.0.0 what's mean all IPv4 addresses on the local machine)
+        :param int port: port, defaults to 8080
+        """
+        super().__init__()
+        self._host: str = host
+        self._port: int = port
+        self._socket = socket.socket()
+
+    def run(self):
+        """
+        Implementation of server.
+        It in infinite loop waits for connection and when the connection will
+        be established receive the data and write it to the file.
+        """
+        self._running = True
+        self._socket.bind((self._host, self._port))
+        self._socket.listen()
+        while self._running:
+            try:
+                conn, addr = self._socket.accept()
+            except ConnectionAbortedError:
+                break
+            received: str = ''
             print('Got connection from ', addr)
             with conn:
                 while True:
                     data = conn.recv(1024)
-                    if not data: break
+                    if not data:
+                        break
                     received += data.decode('utf-8')
             with open('received_file.txt', 'w') as file:
                 file.write(received)
+
+    def stop(self):
+        """
+        Stop the thread loop
+        """
+        self._running = False
+        self._socket.close()
